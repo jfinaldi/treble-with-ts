1e1f070a82cbc6c72b889a73b017d9b09e8d6135
Jennifer <jfinaldi@mail.sfsu.edu>
2021-11-01 11:29:39 -0700

    Fix variable typo and sound button swaps

--- client/src/instruments/Drums.tsx
+++ client/src/instruments/Drums.tsx
@@ -4,19 +4,6 @@ import { Instrument } from "../Instruments";
 function Drums(): JSX.Element {
   const _drum_Beat_one = () => {
     try {
-      fetch("http://localhost:5005/three")
-        .then((response) => response.json())
-        .then((rsp) => {
-          var audioSrc = "data:audio/mp3;base64," + rsp.fileContent;
-          const g = new Tone.Player(audioSrc).toDestination();
-          g.autostart = true;
-        });
-    } catch (e) {
-      console.log("fetch error");
-    }
-  };
-  const cat_mewo = () => {
-    try {
       fetch("http://localhost:5005/two")
         .then((response) => response.json())
         .then((rsp) => {
@@ -26,13 +13,28 @@ function Drums(): JSX.Element {
         });
     } catch (e) {
       console.log("fetch error");
+      throw(e);
+    }
+  };
+  const cat_meow = () => {
+    try {
+      fetch("http://localhost:5005/three")
+        .then((response) => response.json())
+        .then((rsp) => {
+          var audioSrc = "data:audio/mp3;base64," + rsp.fileContent;
+          const g = new Tone.Player(audioSrc).toDestination();
+          g.autostart = true;
+        });
+    } catch (e) {
+      console.log("fetch error");
+      throw(e);
     }
   };
   return (
     <div className="pv4">
       <h3>Drums</h3>
-      <button onClick={_drum_Beat_one}>Cat Meow</button>
-      <button onClick={cat_mewo}>Drum Beat 1</button>
+      <button onClick={_drum_Beat_one}>Drum Beat 1</button>
+      <button onClick={cat_meow}>Cat Meow</button>
     </div>
   );
 }
