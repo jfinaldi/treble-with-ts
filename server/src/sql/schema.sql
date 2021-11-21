8db5acb05736c2fe915b5f92ada944ce4de7077a
asingh26@mail.sfsu.edu <asingh26@mail.sfsu.edu>
2021-11-20 19:04:07 -0800

    tmp share backend

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -9,8 +9,12 @@ PRAGMA foreign_keys;
 CREATE TABLE songs (
 	id int NOT NULL PRIMARY KEY,
 	song_title text NOT NULL,
-	notes varchar NOT NULL
+	notes varchar NOT NULL,
+	instrument varchar NOT NULL
 );
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+INSERT INTO songs (id, song_title, notes, instrument) 
+VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4','piano');
+
+INSERT INTO songs (id, song_title, notes, instrument) 
+VALUES (2, 'Test Track', '4,5,6,7,8','drum');
