1a0dceed8f53eca93c1ce38b8e9998f30bc0c5ce
Jennifer <jfinaldi@mail.sfsu.edu>
2021-11-22 16:55:27 -0800

    Add Jukebox UI to NavBar

--- client/src/SideNav.tsx
+++ client/src/SideNav.tsx
@@ -122,6 +122,32 @@ function Songs({ state, dispatch }: SideNavProps): JSX.Element {
   );
 }
 
+function Player({ state, dispatch}: SideNavProps): JSX.Element {
+  return (
+    <Section title="Jukebox">
+      <div className={classNames('light-gray pl3 pr3 pb3 pt1 dib-ns')}>
+        <div>
+	        <form action="http://www.google.com">
+    	      <input className={classNames('br2-m f4-m tc-l w-75')} 
+                   id="text" type="text" name="song_title" 
+                   placeholder="Search Song by Title"/>
+            <input className={classNames('bg-light-green dib-ns pl00-ns pr00-ns pt00-ns pb00-ns b--diarreen br3 white w-20 f4 tc-ns')}
+                   id="go" type="button" value="Go"></input>
+          </form>    
+        </div>
+        <div className={classNames('tc-ns f4-ns b-m pt2-m pb2-m pl2-m pr2-m black')} 
+             id="song_title">"Ode to Joy"</div>
+        <input className={classNames('b-m green bg-white-60 bsblk br3 pl2 pr2 pt3 pb3 tc no-underline dib-ns f5-ns mt00-m mb00-m ml00-m mr00-m pointer:hover')}
+             id="play" type="button" value="Play"></input>
+        <input className={classNames('b-m black bg-white-60 bsblk br3 pl2 pr2 pt3 pb3 tc no-underline dib-ns f5-ns mt00-m mb00-m ml00-m mr00-m pointer:hover')}
+             id="pause" type="button" value="Pause"></input>
+        <input className={classNames('b-m red bg-white-60 bsblk br3 pl2 pr2 pt3 pb3 tc no-underline dib-ns f5-ns mt00-m mb00-m ml00-m mr00-m pointer:hover')}
+             id="stop" type="button" value="Stop"></input>
+      </div>
+    </Section>
+  );
+}
+
 export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
   return (
     <div className="absolute top-0 left-0 bottom-0 w5 z-1 shadow-1 bg-white flex flex-column">
@@ -132,6 +158,7 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
         <Instruments state={state} dispatch={dispatch} />
         <Visualizers state={state} dispatch={dispatch} />
         <Songs state={state} dispatch={dispatch} />
+        <Player state={state} dispatch={dispatch} />
       </div>
     </div>
   );
